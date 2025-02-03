540437dd055c36dfb74520e49b7c9b5137b8cd54
Zohra <guliyevaazohra@gmail.com>
2025-02-04 00:54:33 +0400

    test commit

---
+++ MOvie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOvie
+//
+//  Created by Zohra Guliyeva  on 04.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
