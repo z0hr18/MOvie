7d9b453ce7f698c4fada7e03f6e4ffdbfd8c00f8
Zohra <guliyevaazohra@gmail.com>
2025-02-07 01:52:08 +0400

    commit
    create tabbar but fix to custom tabbar :)

--- MOvie/HomeViewController.swift
+++ MOvie/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  MOvie
 //
 //  Created by Zohra Guliyeva  on 04.02.25.
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 36/255, green: 42/255, blue: 50/255, alpha: 1.0)
+
     }
 
 
--- MOvie/SceneDelegate.swift
+++ MOvie/SceneDelegate.swift
@@ -8,45 +8,85 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene  = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabbar()
+        window?.makeKeyAndVisible()
     }
-
+    
+    func createHomeNavigationController() -> UINavigationController {
+        let homeViewController = HomeViewController()
+        homeViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        return UINavigationController(rootViewController: homeViewController)
+        
+    }
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchViewController = SearchViewController()
+        searchViewController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        return UINavigationController(rootViewController: searchViewController)
+        
+    }
+    
+    func createWatchListNavigationController() -> UINavigationController {
+        let watchListViewController = WatchListViewController()
+        watchListViewController.tabBarItem = UITabBarItem(title: "Watch List", image: UIImage(systemName: "bookmark"), tag: 2)
+        return UINavigationController(rootViewController: watchListViewController)
+        
+    }
+    
+    func addTabBarTopBorder(tabBar: UITabBar, color: UIColor, height: CGFloat) {
+        let separator = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.width, height: height))
+        separator.backgroundColor = color
+        tabBar.addSubview(separator)
+    }
+    
+    
+    func createTabbar() -> UITabBarController {
+        let tabbar = UITabBarController()
+        UITabBar.appearance().tintColor = .init(named: "0296E5")
+        tabbar.tabBar.backgroundColor = .init(named: "242A32")
+        tabbar.viewControllers = [createHomeNavigationController(), createSearchNavigationController(), createWatchListNavigationController()]
+        addTabBarTopBorder(tabBar: tabbar.tabBar, color: .systemBlue, height: 0.5)
+        
+        return tabbar
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
