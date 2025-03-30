64c93f4d761e4c332360500e7d35a563aa0e44a6
Zohra <guliyevaazohra@gmail.com>
2025-03-30 16:52:31 +0400

    generic network manager

--- MOvie/Services/NetworkManager.swift
+++ MOvie/Services/NetworkManager.swift
@@ -11,7 +11,34 @@ final class NetworkManager {
     static let shared  = NetworkManager()
     private init() {} // kənardan yeni instance yaradılmasın deyə
     
-    func fetch<T: Decodable>(endpoint: Endpoint, completion: @escaping (Result<T, Error>) -> Void) {
+    func fetch<T: Decodable>(endpoint: Endpoint, completion: @escaping (Result<T, Error>) -> Void) { //T Decodable model olacaq; nəticə gələndə completion-u çağıracağıq
+        let request = endpoint.request()
         
+        //URLSession ilə sorğu göndər
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // Burada cavab gələcək
+            
+            // error gelse
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            //data yoxdusa
+            guard let data = data else {
+                completion(.failure(NSError(domain: "Data Error", code: 0)))
+                return
+            }
+            
+            
+            //JSON decode
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(error))
+            }
+            
+        }.resume()
     }
 }
